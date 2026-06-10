8c5aae4cd6a917091a3f515d05ae246850e5931b
reythomas039-art <reythomas039@gmail.com>
2026-06-09 22:41:20 -0300

    Update funciones.js

--- funciones.js
+++ funciones.js
@@ -1,4 +1,19 @@
 function MostrarMensaje ()= {console.log("bienvenido al curso de JavaScript");}
 
 mostrarMensaje()
-mostrarMensaje()
+mostrarMensaje();
+
+function saludar(nombre) {
+  console.log("Hola " + nombre);}
+
+saludar("juan")
+saludar("thomi");
+
+function sumar(a, b) {
+  return a + b;}
+
+let total = sumar(60, 7);
+console.log(total);
+
+<script src="funciones.js"></script>
+
